10357f8c74df94da108a4e10d8b54e03bdb966bb
raghadafghani <raghadafghani@gmail.com>
2024-09-07 10:27:56 +0300

    Initial commit

---
+++ class2.py
@@ -0,0 +1,8 @@
+mylist=["raghad","omar",",maryam"]
+#for x in mylist: # for each element in the list (  )
+    #print(mylist[0])
+print (mylist[0:2])
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
